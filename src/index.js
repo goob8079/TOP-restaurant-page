569475c3a545c954634f1734bd1626b8af06560f
Justice <122542069+goob8079@users.noreply.github.com>
2025-07-10 17:31:26 +0900

    4th commit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import "./styles.css";
 import "@fontsource/varela-round";
+// import "./Burple_nurples.png";
 
 const home = document.querySelector("#home-btn");
 const menu = document.querySelector("#menu-btn");
