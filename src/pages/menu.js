7cd674b84202e8c3ceaeb2eaaa97c7a43b25fe73
Justice <122542069+goob8079@users.noreply.github.com>
2025-07-13 18:04:41 +0900

    updated menu.js and menu.css

--- src/pages/menu.js
+++ src/pages/menu.js
@@ -2,6 +2,12 @@ import "../styles/menu.css";
 import burpleNurplesImg from "../assets/Burple_nurples.png";
 import froggyThumpkinImg from "../assets/Froggy_Apple_Crumple_Thumpkin.png";
 import wontonBombsImg from "../assets/Won_Ton_Bombs.png";
+import meviledEggsImg from "../assets/Meviled_Eggs.png";
+import mineMeachPieImg from "../assets/Mince_Meach_Pie.png";
+import fireSnackerImg from "../assets/Fire_Snacker.png";
+import shickenSoquitosImg from "../assets/Shicken_Soquitos.png";
+import meetAndGreetBallsImg from "../assets/Meet_And_Greet_Balls.png";
+import teddyBearSurpriseIimg from "../assets/Teddy_Bear_Surprise.png";
 
 const content = document.querySelector("#content");
 
@@ -10,8 +16,9 @@ export function menu() {
 
     const firstRow = document.createElement("div");
     firstRow.setAttribute("class", "row1");
+
     const row1Item1 = document.createElement("div");
-    row1Item1.setAttribute("id", "row1-item1");
+    row1Item1.setAttribute("class", "item1");
     const item1P = document.createElement("p");
     item1P.setAttribute("id", "item1-p");
     item1P.textContent = "Burple Nurples";
@@ -24,7 +31,7 @@ export function menu() {
     firstRow.appendChild(row1Item1);
     
     const row1Item2 = document.createElement("div");
-    row1Item2.setAttribute("id", "row1-item2");
+    row1Item2.setAttribute("class", "item2");
     const item2P = document.createElement("p");
     item2P.setAttribute("id", "item2-p");
     item2P.textContent = "Froggy Apple Crumple Thumpkin";
@@ -37,7 +44,7 @@ export function menu() {
     firstRow.appendChild(row1Item2);
 
     const row1Item3 = document.createElement("div");
-    row1Item3.setAttribute("id", "row1-item3");
+    row1Item3.setAttribute("class", "item3");
     const item3P = document.createElement("p");
     item3P.setAttribute("id", "item3-p");
     item3P.textContent = "Won Ton Bombs";
@@ -48,6 +55,92 @@ export function menu() {
     row1Item3.appendChild(item3Img);
     row1Item3.appendChild(item3P);
     firstRow.appendChild(row1Item3);
+
+    const seccondRow = document.createElement("div");
+    seccondRow.setAttribute("class", "row2");
+
+    const row2Item1 = document.createElement("div");
+    row2Item1.setAttribute("class", "item1");
+    const r2Item1P = document.createElement("p");
+    r2Item1P.setAttribute("id", "item1-p");
+    r2Item1P.textContent = "Meviled Eggs";
+    const r2Item1Img = new Image();
+    r2Item1Img.setAttribute("id", "meviled-eggs");
+    r2Item1Img.src = meviledEggsImg;
+    r2Item1Img.alt = "Meviled Eggs";
+    row2Item1.appendChild(r2Item1Img);
+    row2Item1.appendChild(r2Item1P);
+    seccondRow.appendChild(row2Item1);
+
+    const row2Item2 = document.createElement("div");
+    row2Item2.setAttribute("class", "item1");
+    const r2Item2P = document.createElement("p");
+    r2Item2P.setAttribute("id", "item2-p");
+    r2Item2P.textContent = "Mince Meach Pie";
+    const r2Item2Img = new Image();
+    r2Item2Img.setAttribute("id", "mince-meach-pie");
+    r2Item2Img.src = mineMeachPieImg;
+    r2Item2Img.alt = "Mine Meach Pie";
+    row2Item2.appendChild(r2Item2Img);
+    row2Item2.appendChild(r2Item2P);
+    seccondRow.appendChild(row2Item2);
+
+    const row2Item3 = document.createElement("div");
+    row2Item3.setAttribute("class", "item1");
+    const r2Item3P = document.createElement("p");
+    r2Item3P.setAttribute("id", "item1-p");
+    r2Item3P.textContent = "Fire Snacker";
+    const r2Item3Img = new Image();
+    r2Item3Img.setAttribute("id", "fire-snacker");
+    r2Item3Img.src = fireSnackerImg;
+    r2Item3Img.alt = "Fire Snacker";
+    row2Item3.appendChild(r2Item3Img);
+    row2Item3.appendChild(r2Item3P);
+    seccondRow.appendChild(row2Item3);
+
+    const thirdRow = document.createElement("div");
+    thirdRow.setAttribute("class", "row3");
+
+    const row3Item1 = document.createElement("div");
+    row3Item1.setAttribute("class", "item1");
+    const r3Item1P = document.createElement("p");
+    r3Item1P.setAttribute("id", "item1-p");
+    r3Item1P.textContent = "Shicken Soquitos";
+    const r3Item1Img = new Image();
+    r3Item1Img.setAttribute("id", "shicken-soquitos");
+    r3Item1Img.src = shickenSoquitosImg;
+    r3Item1Img.alt = "Shicken Soquitos";
+    row3Item1.appendChild(r3Item1Img);
+    row3Item1.appendChild(r3Item1P);
+    thirdRow.appendChild(row3Item1);
+
+    const row3Item2 = document.createElement("div");
+    row3Item2.setAttribute("class", "item1");
+    const r3Item2P = document.createElement("p");
+    r3Item2P.setAttribute("id", "item2-p");
+    r3Item2P.textContent = "Meet and Greet Balls";
+    const r3Item2Img = new Image();
+    r3Item2Img.setAttribute("id", "meet-balls");
+    r3Item2Img.src = meetAndGreetBallsImg;
+    r3Item2Img.alt = "Meet and Greet Balls";;
+    row3Item2.appendChild(r3Item2Img);
+    row3Item2.appendChild(r3Item2P);
+    thirdRow.appendChild(row3Item2);
+
+    const row3Item3 = document.createElement("div");
+    row3Item3.setAttribute("class", "item1");
+    const r3Item3P = document.createElement("p");
+    r3Item3P.setAttribute("id", "item3-p");
+    r3Item3P.textContent = "Teddy Bear Surprise";
+    const r3Item3Img = new Image();
+    r3Item3Img.setAttribute("id", "teddy-bear-surprise");
+    r3Item3Img.src = teddyBearSurpriseIimg;
+    r3Item3Img.alt = "Teddy Bear Surprise";
+    row3Item3.appendChild(r3Item3Img);
+    row3Item3.appendChild(r3Item3P);
+    thirdRow.appendChild(row3Item3);
     
     content.appendChild(firstRow);
+    content.appendChild(seccondRow);
+    content.appendChild(thirdRow);
 }
